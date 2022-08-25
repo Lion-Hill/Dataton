515efff1484248d8e8a1dda55e545826cfc42ef8
Gyuho Park <pgho8@naver.com>
2022-08-25 10:11:48 +0900

    modify import matplotlib

--- pages/COVID-19_Detection.py
+++ pages/COVID-19_Detection.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import librosa
 import pickle
 import warnings
