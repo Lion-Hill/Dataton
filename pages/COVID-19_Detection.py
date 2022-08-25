edd786065aa50099179f859fbe9247207f1aa883
Gyuho Park <pgho8@naver.com>
2022-08-25 09:51:32 +0900

    220825 first 10:00

--- pages/COVID-19_Detection.py
+++ pages/COVID-19_Detection.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import librosa
 import pickle
+import warnings
+
+warnings.filterwarnings(action='ignore')
 
 st.set_page_config(layout="wide")
 
@@ -43,9 +46,10 @@ def get_mfcc_feature():
 
 
 def data_preprocessing():
-    info_df = pd.DataFrame(data={'age': int(age_info), 'respiratory_condition': symptoms[respiratory_condition],
-                           'fever_or_muscle_pain': symptoms[fever_or_muscle_pain]})
-    sex_df = pd.DataFrame(data={'female': [0], 'male': [0], 'other': [0]})
+    info_df = pd.DataFrame(data={'age': [int(age_info)], 'respiratory_condition': [symptoms[respiratory_condition]],
+                           'fever_or_muscle_pain': [symptoms[fever_or_muscle_pain]]})
+    sex_df = pd.DataFrame(
+        data={'female': [0], 'male': [0], 'other': [0]})
 
     if sex_info == '남성':
         sex_df['male'] = 1
@@ -71,20 +75,15 @@ def judge_covid():
     result_df = pd.merge(mid_df, sex_df, left_index=True, right_index=True)
     # wav_plot(y, sr)
     model = get_model()
-    st.write(result_df)
-    st.write(mfcc_df)
     sample_proba = 0.14
     sample_data = np.array(result_df).reshape(1, -1)
-    st.write(sample_data)
     sample_pred = model.predict_proba(sample_data)[:, 1][0]
-    st.write(sample_pred)
     sample_result = np.where(sample_pred < sample_proba, 0, 1)
-    st.write(sample_result)
     sample_return = np.array(['Negative', 'Positive'])[sample_result]
-    st.write(sample_return)
+    return sample_return
 
 
-audio_file = st.file_uploader("Upload Audio file", type=["wav", "mp3", "mp4"])
+audio_file = st.file_uploader("Upload Audio file", type=["wav", "mp3", "mp4","m4a"])
 age_info = st.number_input(
     'Input your age', min_value=1, max_value=100, step=1)
 sex_info = st.selectbox('성별을 골라주세요', ('남성', '여성', '기타'))
@@ -96,31 +95,28 @@ fever_or_muscle_pain = st.selectbox(
 
 if (audio_file is not None):
     if st.button('Submit audio file'):
-        judge_covid()
+        sample_return = judge_covid()
 
+        if sample_return == 'Negative':
+            st.header('검진결과 코로나 음성입니다!')
+            st.write('개인 방역을 철저히 하시고 경과를 더 지켜봅시다!')
+        else:
+            st.header('검진결과 코로나 양성입니다!')
+            st.write('아래의 안내문을 참고하여 자가격리를 해주세요!')
 
-# covid_result = st.radio('COVID-19 Result', [0, 1])
+            tab1, tab2, tab3 = st.tabs(
+                ["재택치료 안내문(집중관리군)", "재택치료 안내문(일반관리군)", "재택치료자 동거인 안내문"])
+            with tab1:
+                st.header("재택치료 안내문(집중관리군)")
 
-    if covid_result == 0:
-        st.header('검진결과 코로나 음성입니다!')
-        st.write('개인 방역을 철저히 하시고 경과를 더 지켜봅시다!')
-    else:
-        st.header('검진결과 코로나 양성입니다!')
-        st.write('아래의 안내문을 참고하여 자가격리를 해주세요!')
+                st.image('images/재택치료 안내문(집중관리군).jpg')
 
-        tab1, tab2, tab3 = st.tabs(
-            ["재택치료 안내문(집중관리군)", "재택치료 안내문(일반관리군)", "재택치료자 동거인 안내문"])
-        with tab1:
-            st.header("재택치료 안내문(집중관리군)")
+            with tab2:
+                st.header("재택치료 안내문(일반관리군)")
 
-            st.image('images/재택치료 안내문(집중관리군).jpg')
+                st.image('images/재택치료 안내문(일반관리군).jpg')
 
-        with tab2:
-            st.header("재택치료 안내문(일반관리군)")
+            with tab3:
+                st.header("재택치료 동거인 안내문")
 
-            st.image('images/재택치료 안내문(일반관리군).jpg')
-
-        with tab3:
-            st.header("재택치료 동거인 안내문")
-
-            st.image('images/동거인안내문.png')
+                st.image('images/동거인안내문.png')
